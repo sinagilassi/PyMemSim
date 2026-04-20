61f00c6a168b603fa11c2f5cbaca706524923ac5
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-04-20 16:42:21 -0400

    feat: bump version to 0.1.1 in info.py and pyproject.toml

--- pymemsim/configs/info.py
+++ pymemsim/configs/info.py
@@ -4,7 +4,7 @@
 # package name
 __package__ = 'pymemsim'
 # version
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 # description
 __description__ = 'A Python toolkit for modeling of membrane-based gas separation processes.'
 # author
