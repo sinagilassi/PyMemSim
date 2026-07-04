0a74d5e7c8d2d0b02b7f501e0832479452a4b67f
sina.gilassi <sina.gilassi@gmail.com>
2026-07-03 21:49:46 -0400

    feat: implement hydrodynamics functions for fluid dynamics calculations

---
+++ pymemsim/transport/hydrodynamics.py
@@ -0,0 +1,31 @@
+# import libs
+import logging
+from typing import Any, Dict, List, Optional, Tuple, cast
+# locals
+
+# NOTE: logger setup
+logger = logging.getLogger(__name__)
+
+
+def hagen_poiseuille_liquid():
+    pass
+
+
+def hagen_poiseuille_gas_compressible():
+    pass
+
+
+def pressure_drop_fiber_side():
+    pass
+
+
+def pressure_drop_shell_side():
+    pass
+
+
+def reynolds_number():
+    pass
+
+
+def friction_factor_laminar():
+    pass
